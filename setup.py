0c98617347645674e2909c09df1df9ea48ff63f3
Bart <bart.dermul@gmail.com>
2021-01-26 14:56:19 +0100

    upload setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='Kakapo',
     version='0.1',
-    packages=[''],
-    url='',
+    description='a simple python project for EoIP',
+    packages= setuptools.find_packages(),
+    url='https://github.com/drmlbrt/Kakapo',
     license='gerbel license',
     author='DrmlBrt',
-    author_email='bart.dermul@mil.be',
-    description='Setup_project_req'
+    author_email='somwhere@somewhere.com',
+    classifiers=[
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 3 - Alpha',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers - TechBu',
+    'Topic ::  Build Tools for monitoring and Automation',
+
+    # Pick your license as you wish (should match "license" above)
+     'License :: Perso usage',
+
+    # Specify the Python versions you support here. In particular, ensure
+
+    'Programming Language :: Python :: 3.4 and + ',
+    ],
+    python_requires='>=3.6',
 )
