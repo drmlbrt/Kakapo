c2589966392cf90164fca0f937335d53a927c9bc
drmlbrt <bart.dermul@gmail.com>
2021-03-03 14:55:09 +0100

    upload tecex2021

---
+++ TecEx-2021/RESTCONF/restconf-yang-discovery.py
@@ -0,0 +1,25 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+#DisableSSLVerificationWarningbecauseofPrivateSSLCertificate
+
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+AUTH = HTTPBasicAuth("hannibal", "hannibal")
+MEDIA_TYPE = "application/yang-data+json"
+HEADERS = {"Accept": MEDIA_TYPE,
+           "Content-Type": MEDIA_TYPE
+          }
+url = "https://10.242.1.92/restconf/data/Cisco-IOS-XE-native:native/interface"
+
+response=requests.get(url, auth=AUTH, headers=HEADERS, verify=False)
+print("API: ", url)
+print(response.status_code)
+if response.status_code in [200, 202, 204]:
+    print("Successful")
+else:
+    print("Error in API Request")
+print(response.text)
+print("="*40)
+print("="*40)
